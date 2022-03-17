33116525480ce55bed59a4c85e2aba0052be1bc2
HenriqueNascimento <henriquenascivi@gmail.com>
2022-03-16 23:33:00 -0300

    create first contact form rout

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,10 +18,9 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/', 'PrincipalController@principal')->name("site.index");
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name("site.sobrenos");
-
 Route::get('/contato', 'ContatoController@contato')->name("site.contato");
+Route::post('/contato', 'ContatoController@contato')->name("site.contato");
 
 //app
 Route::prefix('/app')->group(function(){
